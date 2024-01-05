8264ad0c23423dca35da9e449c90a143174f5af1
Kassas <104452787+0xcyborg@users.noreply.github.com>
2024-01-05 13:34:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const Element = document.querySelector(".video-stream");
 
 function Set(Value){
   Value = Number(Value);
-  Condition = !isNaN(Value);
-  if(Condition) Element.style.rotate = Value + "deg";
+  if(!isNaN(Value)) Element.style.rotate = Value + "deg";
 }
 
 document.addEventListener('keyup', event => {
